f22d62b87b816eb61f3c4c8a70df604c1dbd6de7
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-07-21 01:26:44 +0330

    Add project files.

---
+++ CleanArc.Domain/Products/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Domain.Products
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; } = string.Empty;
+
+        public Product(string title, decimal price, string description)
+        {
+            Title = title;
+            Price = price;
+            Description = description;
+            Id = Guid.NewGuid();
+            
+        }
+
+        public void Edit(string title, decimal price, string description)
+        {
+            Title = title;
+            Price = price;
+            Description = description;
+
+        }
+    }
+}
